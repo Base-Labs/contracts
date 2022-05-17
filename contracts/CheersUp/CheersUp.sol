2b2c9fe611d92ab6e724eddb8f4fd3b7fdbb2973
storyicon <storyicon@cryptonatty.io>
2022-05-17 16:09:35 +0800

    chore(CheersUp): clean up unused imports
    Signed-off-by: storyicon <storyicon@cryptonatty.io>

--- contracts/CheersUp/CheersUp.sol
+++ contracts/CheersUp/CheersUp.sol
@@ -1,5 +1,4 @@
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
